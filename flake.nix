7cbaf5786b6bbe47053f4ff3bc1a048b89abe606
Rishi Vora <vorarishi22@gmail.com>
2025-05-25 22:45:11 +0530

    feat: add niriswitcher module

--- flake.nix
+++ flake.nix
@@ -16,14 +16,20 @@
 
     packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
 
+    homeManagerModules = import ./modules/home-manager;
+
     devShells = forAllSystems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       default = pkgs.mkShell {
-        packages = with pkgs; [
-          alejandra
-          nvfetcher
-        ];
+        packages =
+          (with pkgs; [
+            alejandra
+            nvfetcher
+          ])
+          ++ (with self.packages.${system}; [
+            niriswitcher
+          ]);
       };
     });
   };
