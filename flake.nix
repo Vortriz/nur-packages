24b32f557177aca7553941981d7b696390671000
Rishi Vora <vorarishi22@gmail.com>
2025-05-26 00:18:35 +0530

    feat: configure pre-commit tweak: limit systems feat: configure justfile

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+        pre-commit-hooks = {
+            url = "github:cachix/git-hooks.nix";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
         treefmt-nix = {
             url = "github:numtide/treefmt-nix";
             inputs.nixpkgs.follows = "nixpkgs";
@@ -15,7 +20,7 @@
         nixpkgs,
         ...
     } @ inputs: let
-        forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+        forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux"];
         forAllPkgs = f: forAllSystems (system: f nixpkgs.legacyPackages.${system});
         treefmtEval = forAllPkgs (pkgs: inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
     in {
@@ -26,6 +31,23 @@
 
         formatter = forAllPkgs (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
 
+        checks = forAllSystems (system: {
+            pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+                src = ./.;
+                hooks = {
+                    flake-checker = {
+                        enable = true;
+                        after = ["treefmt-nix"];
+                    };
+                    treefmt-nix = {
+                        enable = true;
+                        entry = "${treefmtEval.${system}.config.build.wrapper}/bin/treefmt";
+                        pass_filenames = false;
+                    };
+                };
+            };
+        });
+
         packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
 
         homeManagerModules = import ./modules/home-manager;
@@ -34,13 +56,17 @@
             pkgs = nixpkgs.legacyPackages.${system};
         in {
             default = pkgs.mkShell {
+                inherit (self.checks.${system}.pre-commit-check) shellHook;
+
                 buildInputs = [
+                    self.checks.${system}.pre-commit-check.enabledPackages
                     treefmtEval.${system}.config.build.wrapper
                 ];
 
                 packages =
                     (with pkgs; [
                         alejandra
+                        just
                         nvfetcher
                     ])
                     ++ (with self.packages.${system}; [
