7b0148285dbc809fe2f857535c22d65e0ae1b547
Vortriz <vorarishi22+github@gmail.com>
2025-12-16 01:26:37 +0530

    feat: migrate espanso packages from Vortriz/dotfiles

--- default.nix
+++ default.nix
@@ -33,6 +33,10 @@ in
     directory = ./pkgs/by-name;
 })
 // (pkgs.lib.packagesFromDirectoryRecursive {
+    inherit (pkgs) callPackage;
+    directory = ./pkgs/espansoPackages;
+})
+// (pkgs.lib.packagesFromDirectoryRecursive {
     callPackage = pkgs.lib.callPackageWith (pkgs // { inherit (pkgs.yaziPlugins) mkYaziPlugin; });
     directory = ./pkgs/yaziPlugins;
 })
